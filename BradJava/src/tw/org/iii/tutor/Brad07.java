4ad9ec243c489869bad3ad97f8051f68225ea775
User <User@R1802.r1802.tciii.org.tw>
2023-06-13 16:30:24 +0800

    1

--- BradJava/src/tw/org/iii/tutor/Brad07.java
+++ BradJava/src/tw/org/iii/tutor/Brad07.java
@@ -5,21 +5,21 @@ public class Brad07 {
 	public static void main(String[] args) {
 
 		double temp = Math.random();
-		int score = (int)(temp*101);
+		int score = (int) (temp * 101);
 		System.out.println(score);
-		
+
 		if (score >= 90) {
 			System.out.println("A");
-		}else if (score >= 80) {
+		} else if (score >= 80) {
 			System.out.println("B");
-		}else if (score >= 70) {
+		} else if (score >= 70) {
 			System.out.println("C");
-		}else if (score >= 60) {
+		} else if (score >= 60) {
 			System.out.println("D");
-		}else {
+		} else {
 			System.out.println("E");
 		}
-		
+
 		System.out.println("Game Over");
 	}
 
