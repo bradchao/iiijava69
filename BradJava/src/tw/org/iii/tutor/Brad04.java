7957b2b63850c69b2823ab86f2cb2675578c1136
User <User@R1802.r1802.tciii.org.tw>
2023-06-13 14:37:15 +0800

    06131437

---
+++ BradJava/src/tw/org/iii/tutor/Brad04.java
@@ -0,0 +1,19 @@
+package tw.org.iii.tutor;
+
+public class Brad04 {
+
+	public static void main(String[] args) {
+		float var1 = 10.123456789f;	
+		double var2 = 10.123456789;
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		double a = 0;
+		double b= 0;
+		System.out.println(a / b);
+		
+		System.out.println(10 / 0);
+		
+	}
+
+}
