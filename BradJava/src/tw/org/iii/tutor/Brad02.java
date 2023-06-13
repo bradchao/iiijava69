89439ceb5a1bb848c07620fdca9d1b4e0ad971f2
User <User@R1802.r1802.tciii.org.tw>
2023-06-13 13:31:32 +0800

    06131331

--- BradJava/src/tw/org/iii/tutor/Brad02.java
+++ BradJava/src/tw/org/iii/tutor/Brad02.java
@@ -3,7 +3,7 @@ package tw.org.iii.tutor;
 public class Brad02 {
 
 	public static void main(String[] args) {
-		byte var1 = 12;
+		byte var1 = 15;
 		byte var2 = 13;
 		byte var21 = (byte)(var1 + var2);
 		System.out.println(var21);
