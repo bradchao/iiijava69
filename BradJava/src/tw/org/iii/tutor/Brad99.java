ef1055f0b17a8ff6b00a5392868340fbbe4aae34
User <User@R1802.r1802.tciii.org.tw>
2023-06-14 11:19:43 +0800

    06141119

--- BradJava/src/tw/org/iii/tutor/Brad99.java
+++ BradJava/src/tw/org/iii/tutor/Brad99.java
@@ -3,12 +3,16 @@ package tw.org.iii.tutor;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		for (int j=1; j<=9; j++) {
-			for (int i=2; i<=5; i++) {
-				int r = i * j;
-				System.out.printf("%d x %d = %d\t", i, j, r);
+		for (int k=0; k<2; k++) {
+			for (int j=1; j<=9; j++) {
+				for (int i=2; i<=5; i++) {
+					int newi = i + k*4;
+					int r = newi * j;
+					System.out.printf("%d x %d = %d\t", newi, j, r);
+				}
+				System.out.println();
 			}
-			System.out.println();
+			System.out.println("---");
 		}
 	}
 
