d392da6445e2e2cfa18550c20070f6860ef435c5
User <User@R1802.mshome.net>
2023-06-26 08:53:11 +0800

    06260853

--- BradJava/src/tw/org/iii/tutor/Brad41.java
+++ BradJava/src/tw/org/iii/tutor/Brad41.java
@@ -1,10 +1,35 @@
 package tw.org.iii.tutor;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Brad41 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Brad411 obj = new Brad411();
+		obj.m1();
 	}
-
 }
+class Brad411 {
+	void m1() throws Exception {
+		m2();
+	}
+	void m2() throws InterruptedException {
+		try {
+			m3(1);
+		}catch(IOException e) {
+			
+		}
+	}
+	void m3(int a) throws IOException, InterruptedException {
+		if (a == 1) {
+			throw new IOException();
+		}else if (a == 2) {
+			throw new InterruptedException();
+		}else if (a == 3) {
+			throw new FileNotFoundException();
+		}
+	}
+			
+	
+}
+
